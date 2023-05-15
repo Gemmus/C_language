329664d191d796d3776a2a17b1fbe7b441d0778c
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-15 15:18:25 +0300

    Create 03_commented_out.c

---
+++ 03_commented_out.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main3(void) {
+	printf("\"Blahblahblah\" - XY");
+
+	// For one line comments
+
+	/* For
+	 * multiple
+	 * line
+	 * comments
+	   */
+
+	return 0;
+}
