5b892fd398d4103c89e928d42c1fac7dca355a6a
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-18 15:59:05 +0300

    Add files via upload

---
+++ 13_hypotenuse_calculator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+	double a, b, c;
+
+	printf("Enter side \"a\" in metre:  ");
+	scanf("%lf", &a);
+
+	printf("Enter side \"b\"  in metre:  ");
+	scanf("%lf", &b);
+
+	c = sqrt(pow(a, 2) + pow(b, 2));
+
+	printf("The hypotenuse is %.1lf m.", c);
+
+	return 0;
+}
