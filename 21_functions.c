5a26f48f3ef83f0264bc134700085ed3e2f5831b
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-19 10:56:49 +0300

    Add files via upload

---
+++ 21_functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void birthday21(){
+	printf("Happy Birthday to You!\n");
+	printf("Happy Birthday to You!\n");
+	printf("Happy Birthday Dear You!\n");
+	printf("Happy Birthday to You!\n\n");
+}
+
+int main21(void) {
+	birthday();
+	birthday();
+	birthday();
+	return 0;
+}
