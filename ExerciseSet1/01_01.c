80ddac64deb3bbf4d8bc6a86111bae8034310468
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-29 08:13:58 +0300

    Create 01_01.c

---
+++ ExerciseSet1/01_01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main1(void){
+
+	int number;
+
+	printf("Enter an integer number:  ");
+	scanf("%d", &number);
+
+	printf("Your integer number was %d.", number);
+
+	return 0;
+}
