cc27b084d9925f3c72566aed374c109fda422d26
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-29 14:46:50 +0300

    Create 01_triangle_area.c

---
+++ ExerciseSet2/01_triangle_area.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+	double a;
+	double b;
+	double c;
+
+	printf("Enter side a:  ");
+	scanf("%lf", &a);
+
+	printf("Enter side a:  ");
+	scanf("%lf", &b);
+
+	printf("Enter side a:  ");
+	scanf("%lf", &c);
+
+	if((a + b) > c && (b + c) > a && (a + c) > b){
+		double semiperimeter = (a + b + c ) / 2;
+		double area = sqrt(semiperimeter * (semiperimeter - a)  *(semiperimeter - b) * (semiperimeter - c));
+		printf("The area of the triangle is: %.2lf squaremetres.\n", area);
+	}
+	else{
+		printf("The triangle does not exist with given parameters.\n");
+	}
+
+	return 0;
+}
