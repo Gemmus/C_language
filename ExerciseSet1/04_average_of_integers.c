f7040fb182d3e671af983cdf2c4a0f19f43a8730
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-05-29 17:13:42 +0300

    Update 04_average_of_integers.c

--- ExerciseSet1/04_average_of_integers.c
+++ ExerciseSet1/04_average_of_integers.c
@@ -4,27 +4,19 @@
 
 int main(void){
 
-	int integers[10];
-	int input;
-	int size = 0;
-	float total;
-	float average;
+	int count;
+	float number, total = 0, average;
 
-	do{
-		printf("Enter an integer or 0 to quit:  ");
-		int retval = scanf("%d", &input);
-		if(retval == 1 && input != 0){
-			integers[size] = input;
-			size++;
-		}
-	}while(size < 10 && input != 0);
+	printf("Enter the number of inputs:  ");
+	scanf("%d", &count);
 
-	for(int i = 0; i < size; i++){
-		//printf("%d ", integers[i]);
-		total += integers[i];
+	for(int i = 0; i < count; i++){
+		printf("Enter the number:  ");
+		scanf("%f", &number);
+		total += number;
 	}
 
-	average = total/size;
+	average = total/count;
 	printf("The average of entered numbers is %.2f.", average);
 
 	return 0;
