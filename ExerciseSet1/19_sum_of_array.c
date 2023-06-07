9876074a0cfe5434b60d8b48ea4c3d9d3a010bbe
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-07 05:11:38 +0300

    Add files via upload

---
+++ ExerciseSet1/19_sum_of_array.c
@@ -0,0 +1,31 @@
+// C program to display the sum of n numbers using an array
+
+#include <stdio.h>
+
+int main19(void){
+
+	int integers[10];
+	int input;
+	int size = 0;
+	float total;
+	float average;
+
+	do{
+		printf("Enter an integer or 0 to quit:  ");
+		int retval = scanf("%d", &input);
+		if(retval == 1 && input != 0){
+			integers[size] = input;
+			size++;
+		}
+	}while(size < 10 && input != 0);
+
+	for(int i = 0; i < size; i++){
+		//printf("%d ", integers[i]);
+		total += integers[i];
+	}
+
+	average = total/size;
+	printf("The average of entered numbers is %.2f.", average);
+
+	return 0;
+}
