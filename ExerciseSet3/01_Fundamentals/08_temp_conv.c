55330fcf5f6aaade49c06501daeaf5250cd722ee
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-13 11:53:29 +0300

    Create 08_temp_conv.c

---
+++ ExerciseSet3/01_Fundamentals/08_temp_conv.c
@@ -0,0 +1,22 @@
+/*
+Write a C program to input temperature in Centigrade and convert to Fahrenheit.
+How to convert temperature from degree centigrade to degree Fahrenheit in C programming.
+Logic to convert temperature from Celsius to Fahrenheit in C.
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	double celcius, fahrenheit;
+
+	printf("Enter the value in centimeters:  ");
+	scanf("%lf", &celcius);
+
+	fahrenheit = (celcius * 9 / 5) + 32;
+
+	printf("%.2lf Celcius is %.2lf Fahrenheit.\n", celcius, fahrenheit);
+
+	return 0;
+}
+
