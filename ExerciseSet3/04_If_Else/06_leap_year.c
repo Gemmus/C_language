7affd0607742cadd552e3c124d8f1b0ed54dbf0b
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-19 11:05:13 +0300

    Create 06_leap_year.c

---
+++ ExerciseSet3/04_If_Else/06_leap_year.c
@@ -0,0 +1,35 @@
+/*
+Write a C program to check leap year using if else. 
+How to check whether a given year is leap year or not in C programming. 
+C Program to input year from user and check whether the given year is leap year or not using ladder if else. 
+Logic to check leap year in C programming.
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+	int year;
+
+	printf("Enter a year:  ");
+	scanf("%d", &year);
+
+	if(year % 4 == 0){
+		if(year % 100 != 0){
+			printf("%d is a leap year.", year);
+		}
+		else{
+			if(year % 400 == 0){
+				printf("%d is a leap year.", year);
+			}
+			else {
+				printf("%d is not a leap year.", year);
+			}
+		}
+	}
+	else {
+		printf("%d is not a leap year.", year);
+	}
+
+	return 0;
+}
