0c482584795d7dde6b1eb5996bb95a4d452b6933
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-21 09:56:25 +0300

    Create 03_printing _a_to_z.c

---
+++ ExerciseSet3/06_Loop_Iteration/03_printing _a_to_z.c
@@ -0,0 +1,14 @@
+/*
+Write a C program to print alphabets from a to z using for loop.
+How to print alphabets using loop in C programming.
+Logic to print alphabets from a to z using for loop in C programming.
+*/
+
+#include <stdio.h>
+
+int main(){
+	
+	for(char ch = 'a'; ch <= 'z'; ch++) printf("%c ", ch);
+
+	return 0;
+}
