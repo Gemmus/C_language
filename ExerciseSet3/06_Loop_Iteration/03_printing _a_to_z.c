4ff00e6f3a8ad2c3359c4cee3e9714aea44e8b5a
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-21 09:58:31 +0300

    Added while loop.

--- ExerciseSet3/06_Loop_Iteration/03_printing _a_to_z.c
+++ ExerciseSet3/06_Loop_Iteration/03_printing _a_to_z.c
@@ -7,8 +7,17 @@ Logic to print alphabets from a to z using for loop in C programming.
 #include <stdio.h>
 
 int main(){
-	
+
 	for(char ch = 'a'; ch <= 'z'; ch++) printf("%c ", ch);
 
+	//while loop:
+	/*
+	char ch_w= 'a';
+	while(ch_w <= 'z'){
+		printf("%c ", ch_w);
+		ch_w++;
+	}
+	*/
+	
 	return 0;
 }
