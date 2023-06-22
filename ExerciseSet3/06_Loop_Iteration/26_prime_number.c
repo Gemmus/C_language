19141ff566eb83e38c3b3f3f48794be3354a6587
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-22 11:16:33 +0300

    Create 26_prime_number.c

---
+++ ExerciseSet3/06_Loop_Iteration/26_prime_number.c
@@ -0,0 +1,27 @@
+/*
+Write a program in C to input a number and check whether the number is prime number or not using for loop.
+How to check prime numbers using loop in C programming. Logic to check prime numbers in C programming.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int number, prime = 1;
+
+    printf("Enter the number:\n");
+    scanf("%d", &number);
+
+    for(int i = 2; i < (number / 2); i++)
+    {
+    	if(number % i == 0){
+    		prime = 0;
+    		break;
+    	}
+    }
+
+    if(prime == 1) printf("%d is a prime number.", number);
+    else printf("%d is not a prime number.", number);
+
+    return 0;
+}
