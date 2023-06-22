454ee16c38e5257397a1bfaa5c4331aae1a15957
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-22 14:03:15 +0300

    Create 04_odd_even.c

---
+++ ExerciseSet3/07_Functions/04_odd_even.c
@@ -0,0 +1,31 @@
+/*
+Write a C program to input a number from user and check whether given number is even or odd using functions.
+How to check even or odd using functions in C programming. Write a function in C programming to check even or odd.
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int oddChecker(int num);
+
+int main()
+{
+    int num, odd;
+
+    printf("Enter a number:\n");
+    scanf("%d", &num);
+
+    odd = oddChecker(num);
+
+    if(odd == 1) printf("%d is an odd number.\n", num);
+    else printf("%d is the even number.", num);
+
+    return 0;
+}
+
+int oddChecker(int num) {
+	int odd;
+	if(num % 2 == 0) odd = 0;
+	else odd = 1;
+	return odd;
+}
