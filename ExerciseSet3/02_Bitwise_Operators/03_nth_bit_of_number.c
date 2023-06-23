e3fa42236d770e5184737eb41cc4ad454d01212d
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-23 08:40:33 +0300

    Update 03_nth_bit_of_number.c

--- ExerciseSet3/02_Bitwise_Operators/03_nth_bit_of_number.c
+++ ExerciseSet3/02_Bitwise_Operators/03_nth_bit_of_number.c
@@ -2,6 +2,12 @@
 Write a C program to input any number from user and check whether nth bit of the given number is set (1) or not (0).
 How to check whether nth bit of a given number is set or unset using bitwise operator in C programming.
 C program to get the status of nth bit of a number.
+
+Steps:
+
+1. Input number from user. Store it in some variable say num.
+2. Input the bit position from user. Store it in some variable say n.
+3. To get the nth bit of num right shift num, n times. Then perform bitwise AND with 1 i.e. bitStatus = (num >> n) & 1;.
 */
 
 #include <stdio.h>
