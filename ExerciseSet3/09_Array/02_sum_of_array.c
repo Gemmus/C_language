cf2291cfb681f2a771dec87804aebb78394dd5d8
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-24 12:38:25 +0300

    Create 02_sum_of_array.c

---
+++ ExerciseSet3/09_Array/02_sum_of_array.c
@@ -0,0 +1,25 @@
+/*
+Write a C program to read elements in an array and find the sum of array elements.
+C program to find sum of elements of the array.
+How to add elements of an array using for loop in C programming.
+Logic to find sum of array elements in C programming.
+*/
+
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int main()
+{
+    int array[MAX_SIZE], size, sum = 0;
+
+    printf("Enter the number of the elements:\n");
+    scanf("%d", &size);
+
+    printf("Enter the elements:\n");
+    for(int i = 0; i < size; i++) scanf("%d", &array[i]);
+
+    for(int i = 0; i < size; i++) sum += array[i];
+    printf("The sum of the array is: %d.", sum);
+
+    return 0;
+}
