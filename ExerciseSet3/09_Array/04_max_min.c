92882fa8f2184e6ed866d7c6e027252669d9fbe8
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-27 12:58:37 +0300

    Update 04_max_min.c

--- ExerciseSet3/09_Array/04_max_min.c
+++ ExerciseSet3/09_Array/04_max_min.c
@@ -1,1 +1,31 @@
-http://web.archive.org/web/20220818004528/https://codeforwin.org/2015/07/array-programming-exercises-and.html
+/*
+Write a C program to input elements in an array from user, find maximum and minimum element in array.
+C program to find biggest and smallest elements in an array.
+Logic to find maximum and minimum element in array in C programming.
+*/
+
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int main()
+{
+    int array[MAX_SIZE], size;
+
+    printf("Enter the number of the elements:\n");
+    scanf("%d", &size);
+
+    printf("Enter the elements:\n");
+    for(int i = 0; i < size; i++) scanf("%d", &array[i]);
+
+    int min = array[0], max = array[0];
+
+    for(int i = 0; i < size; i++) {
+    	if(array[i] > max) max = array[i];
+    	if(array[i] < min) min = array[i];
+    }
+
+    printf("The smallest of the entered numbers is: %d.\n", min);
+    printf("The largest of the entered numbers is: %d.\n", max);
+
+    return 0;
+}
