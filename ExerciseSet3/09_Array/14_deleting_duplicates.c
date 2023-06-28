170cb042b6fcf283b3f15d1a6e67d6a36ba4634f
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-28 18:28:43 +0300

    Create 14_deleting_duplicates.c

---
+++ ExerciseSet3/09_Array/14_deleting_duplicates.c
@@ -0,0 +1,40 @@
+/*
+Write a C program to delete duplicate elements from array.
+How to remove duplicate elements from array in C programming.
+After performing delete operation the array should only contain unique integer value.
+Logic to delete duplicate elements from array.
+*/
+
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int main()
+{
+    int array[MAX_SIZE], size;
+
+    printf("Enter the number of the elements:\n");
+    scanf("%d", &size);
+    printf("Enter the elements:\n");
+    for(int i = 0; i < size; i++) scanf("%d", &array[i]);
+
+    for(int i = 0; i < size; i++)
+    {
+    	for(int j = i + 1; j < size; j++)
+        {
+    		if(array[i] == array[j])
+            {
+    			for(int k = j; k < size - 1; k++)
+                {
+    				array[k] = array[k + 1];
+                }
+                size--;
+                j--;
+                }
+            }
+        }
+
+    printf("Array elements after deleting duplicates:\n");
+    for(int i = 0; i < size; i++) printf("%d; ", array[i]);
+
+    return 0;
+}
