b21058a4fdba6b3353a2e8d81014599312acc197
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-06-29 05:51:19 +0300

    Create 03_scalar_multiplication.c

---
+++ ExerciseSet3/10_2D_Array/03_scalar_multiplication.c
@@ -0,0 +1,45 @@
+/*
+Write a C program to read elements in a matrix and perform scalar multiplication of matrix.
+C program for scalar multiplication of matrix.
+How to perform scalar matrix multiplication in C programming. Logic to perform scalar matrix multiplication in C program.
+*/
+
+#include <stdio.h>
+#define SIZE 3
+
+int main()
+{
+    int array1[SIZE][SIZE], array2[SIZE][SIZE], row, column, scalar;
+
+    printf("Enter elements in matrix A(size: 3 x 3):\n");
+    for(row = 0; row < SIZE; row++)
+    {
+        for(column = 0; column < SIZE; column++)
+        {
+            scanf("%d", &array1[row][column]);
+        }
+    }
+
+    printf("Enter elements in matrix A(size: 3 x 3):\n");
+    scanf("%d", &scalar);
+
+
+    for(row = 0; row < SIZE; row++)
+    {
+        for(column = 0; column < SIZE; column++)
+        {
+            array2[row][column] = array1[row][column] * scalar;
+        }
+    }
+
+    for(row = 0; row < SIZE; row++)
+    {
+    	printf("\n");
+        for(column = 0; column < SIZE; column++)
+        {
+            printf("%d ", array2[row][column]);
+        }
+    }
+
+    return 0;
+}
