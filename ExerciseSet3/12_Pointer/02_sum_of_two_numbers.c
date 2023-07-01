82227233b02140cdd8a754ba8b373303990b8b75
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-01 12:50:04 +0300

    Create 02_sum_of_two_numbers.c

---
+++ ExerciseSet3/12_Pointer/02_sum_of_two_numbers.c
@@ -0,0 +1,25 @@
+/*
+Write a C program to read two numbers from user and add them using pointers.
+How to find sum of two number using pointers in C programming.
+Program to perform arithmetic operations on number using pointers.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int num1, num2;
+	int * ptr1, * ptr2;
+
+	printf("Enter first number:\n");
+	scanf("%d", &num1);
+	printf("Enter second number:\n");
+	scanf("%d", &num2);
+
+	ptr1 = &num1;
+	ptr2 = &num2;
+
+	printf("Sum of the two numbers: %d.", *ptr1 + *ptr2);
+
+  return 0;
+}
