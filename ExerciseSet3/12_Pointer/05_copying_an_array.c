3e9e85789d4669fbfada1d3e2f1c8a72ae19a331
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-03 10:37:23 +0300

    Create 05_copying_an_array.c

---
+++ ExerciseSet3/12_Pointer/05_copying_an_array.c
@@ -0,0 +1,39 @@
+/*
+Write a C program to copy one array elements to another array using pointers.
+How to copy array elements from one array to another array using pointers.
+Logic to copy one array to another array using pointers in C programming.
+*/
+
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int main()
+{
+	int array1[MAX_SIZE], size, array2[MAX_SIZE];
+	int * ptr1 = array1, * ptr2 = array2;
+
+	printf("Enter the number of elements:\n");
+	scanf("%d", &size);
+	printf("Enter each of the elements:\n");
+	for(int i = 0; i < size; i++){
+		scanf("%d", ptr1 + i);
+	}
+
+	ptr1 = array1;
+
+	for (int i = 0; i < size; i++){
+		*ptr2 = *ptr1;
+		ptr1++;
+		ptr2++;
+	}
+
+	ptr2 = array2;
+
+	printf("Elements of the copied array: ");
+	for (int i = 0; i < size; i++){
+		printf("%d, ", *ptr2);
+		ptr2++;
+	}
+
+    return 0;
+}
