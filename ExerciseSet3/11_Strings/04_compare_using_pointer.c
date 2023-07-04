66216515949ec1c8320515beb3d801626b094c52
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-04 10:58:07 +0300

    Create 04_compare_using_pointer.c

---
+++ ExerciseSet3/11_Strings/04_compare_using_pointer.c
@@ -0,0 +1,36 @@
+/*
+Write a C program to compare two strings using loop character by character.
+How to compare two strings without using inbuilt library function strcmp() in C programming.
+Comparing two strings lexicographically without using string library functions.
+How to compare two strings using strcmp() library function.
+*/
+
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int compare(char * str1, char * str2);
+
+int main()
+{
+	char input1[MAX_SIZE], input2[MAX_SIZE];
+	int result;
+
+	printf("Enter first string:\n");
+	gets(input1);
+	printf("Enter second string:\n");
+	gets(input2);
+
+	result = compare(input1, input2);
+
+	if(result == 0) printf("Both strings are equal.");
+	else if(result < 0) printf("First string is lexicographically smaller than second.");
+	else printf("First string is lexicographically greater than second.");
+	return 0;
+	}
+
+int compare(char * str1, char * str2)
+{
+	while((*str1 && *str2) && (*str1 == *str2)) { str1++; str2++; }
+
+	return *str1 - *str2;
+}
