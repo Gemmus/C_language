82329faff5a43256cbe0dff3cb33291a4020c56e
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-07-07 13:25:02 +0300

    Create 16_reverse_string.c

---
+++ ExerciseSet3/12_Pointer/16_reverse_string.c
@@ -0,0 +1,32 @@
+/*
+Write a C program to find reverse of a given string using pointer.
+*/
+
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int main()
+{
+	char input[MAX_SIZE], reverse[MAX_SIZE];
+	char *iPtr = input;
+	char *rPtr = reverse;
+	int len = 0;
+
+	printf("Enter your text:\n");
+	gets(input);
+
+	while(*(iPtr++)) len++;
+
+	iPtr--;
+	while(len >= 0)
+	{
+		*(rPtr++) = *(--iPtr);
+	    len--;
+	}
+
+	*rPtr = '\0';
+
+	printf("Reverse string: %s.", reverse);
+
+	return 0;
+}
