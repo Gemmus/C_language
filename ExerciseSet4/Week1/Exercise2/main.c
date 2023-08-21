2bd9e736ef1c5ffef02394bb6600fb4fd38c8c35
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-08-21 19:09:08 +0300

    Changed variable names.

--- ExerciseSet4/Week1/Exercise2/main.c
+++ ExerciseSet4/Week1/Exercise2/main.c
@@ -4,8 +4,8 @@
 int main(void)
 {
     double tax_rate = 0, income_limit = 0, excessive_tax_rate = 0, old_total = 0, new_total = 0;
-    double array1[MAX_SIZE], array2[MAX_SIZE];
-    int first = 0;
+    double income_array[MAX_SIZE], tax_array[MAX_SIZE];
+    int reached_limit = 0;
 
     printf("Enter tax rate: ");
     scanf("%lf", &tax_rate);
@@ -18,18 +18,18 @@ int main(void)
 
     for(int i = 0; i < MAX_SIZE; i++) {
         printf("Enter the income for month %d: ", i + 1);
-        scanf("%lf", &array1[i]);
-        new_total += array1[i];
+        scanf("%lf", &income_array[i]);
+        new_total += income_array[i];
         if (new_total <= income_limit) {
-            array2[i] = array1[i] * tax_rate / 100;
+            tax_array[i] = income_array[i] * tax_rate / 100;
         }
         else {
-            if (first == 0){
-                array2[i] = ((income_limit - old_total) * tax_rate + (new_total - income_limit) * excessive_tax_rate) / 100;
-                first = 1;
+            if (reached_limit == 0){
+                tax_array[i] = ((income_limit - old_total) * tax_rate + (new_total - income_limit) * excessive_tax_rate) / 100;
+                reached_limit = 1;
             }
             else {
-                array2[i] = array1[i] * excessive_tax_rate / 100;
+                tax_array[i] = income_array[i] * excessive_tax_rate / 100;
             }
         }
         old_total = new_total;
@@ -38,7 +38,7 @@ int main(void)
     printf("\nmonth      income         tax\n");
 
     for(int j = 0; j < MAX_SIZE; j++) {
-        printf(" %4d %11.2f %11.2f\n", j + 1, array1[j], array2[j]);
+        printf(" %4d %11.2f %11.2f\n", j + 1, income_array[j], tax_array[j]);
     }
 
     return 0;
