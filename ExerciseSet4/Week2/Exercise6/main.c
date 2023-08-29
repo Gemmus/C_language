7f7638f5b8c6466d9038f50b043d785ac664b690
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-08-29 18:16:38 +0300

    Update main.c

--- ExerciseSet4/Week2/Exercise6/main.c
+++ ExerciseSet4/Week2/Exercise6/main.c
@@ -2,18 +2,42 @@
 #include <stdlib.h>
 #include <time.h>
 
+int input_validator(int low, int high);
+int roll_dice(int highest_value);
 void roll_printer(int highest_value, int random_num);
-void roll_dice(int highest_value);
 
 int main(void)
 {
-    unsigned int selection = 0;
-    const int smaller_dice = 6, bigger_dice = 10;
+    int selection = 0, dice_number = 0;
+    const int smaller_dice = 6, bigger_dice = 10, low = 0, high = 3;
 
     printf("Select from the following operations:\n"
            "Roll D%d --> Enter: 1\n"
-            "Roll D%d --> Enter: 2\n"
+           "Roll D%d --> Enter: 2\n"
            "Quit --> Enter: 3\n", smaller_dice, bigger_dice);
+
+    selection = input_validator(low, high);
+
+    switch(selection) {
+        case 1:
+            dice_number = roll_dice(smaller_dice);
+            roll_printer(smaller_dice, dice_number);
+            break;
+        case 2:
+            dice_number = roll_dice(bigger_dice);
+            roll_printer(bigger_dice, dice_number);
+            break;
+        case 3:
+            printf("You selected quit. Bye!\n");
+            break;
+    }
+
+    return 0;
+}
+
+int input_validator(int low, int high)
+{
+    int selection = 0;
 
     do {
 
@@ -24,25 +48,19 @@ int main(void)
             printf("Invalid input. ");
         }
 
-        else if ((selection <= 0) || (selection > 3)) {
+        else if ((selection <= low) || (selection > high)) {
             printf("Out of range! ");
         }
 
-    } while ((selection <= 0) || (selection > 3));
+    } while ((selection <= low) || (selection > high));
 
-    switch(selection) {
-        case 1:
-            roll_dice(smaller_dice);
-            break;
-        case 2:
-            roll_dice(bigger_dice);
-            break;
-        case 3:
-            printf("You selected quit. Bye!\n");
-            break;
-    }
+    return selection;
+}
 
-    return 0;
+int roll_dice(int highest_value)
+{
+    srand(time(0));
+    return (rand() % highest_value) + 1;
 }
 
 void roll_printer(int highest_value, int random_num)
@@ -50,10 +68,4 @@ void roll_printer(int highest_value, int random_num)
     printf("Rolling dice D%d...\n"
            "...\n"
            "You rolled: %d.\n", highest_value, random_num);
-}
-
-void roll_dice(int highest_value)
-{
-    srand(time(0));
-    roll_printer(highest_value, (rand() % highest_value) + 1);
 }
