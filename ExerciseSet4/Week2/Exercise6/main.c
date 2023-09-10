3598cc152dbec90100f5fbcb6447a6d0943e6f5a
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-09-10 08:19:14 +0300

    Update main.c

--- ExerciseSet4/Week2/Exercise6/main.c
+++ ExerciseSet4/Week2/Exercise6/main.c
@@ -1,3 +1,16 @@
+/*
+Write a program that prints a menu and asks user to select an operation. The operations are:
+• Roll D6
+• Roll D10
+• Quit
+
+Input must be validated and a descriptive error message must be printed if input is invalid. Rolling
+D6 prints a random number in the range of 1 – 6 and rolling D10 prints a random number in the
+range of 1 – 10.
+
+Use functions to structure your program.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
