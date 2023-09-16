5a7f7e5e0d8923e8fca1047d514c8764aad259b2
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-09-16 07:46:02 +0300

    Create main.c

---
+++ MiniProject/Exercise1/main.c
@@ -0,0 +1,21 @@
+/*
+Transcript printer
+Write a program that reads course information from a file. The file is a text file where each line
+contains either course name and number of credits in square brackets or student name and grade
+separated with a colon. For example:
+
+[Computer architecture:5]
+Billie Mandala: 5
+Arthur Dent: 3
+Gabriel José de la Concordia García Márquez: 2
+[C programming:3]
+Olive Nelson: 0
+Dilbert Einstein: 5
+Arthur Dent: 4
+
+Empty lines are ignored. White space at the beginning or end of the student name or course title is
+removed. Names that appear before any course title are ignored and error message is printed.
+  
+Program asks user to enter students name and then prints a list of courses that the student has taken
+with grades and finally the average grade. If user enters “stop” as the name the program stops.
+*/
