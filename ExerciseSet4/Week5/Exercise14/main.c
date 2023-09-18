d6161a32ee2c708397b90cb2116c84d879683ca2
Gemmus <112064697+Gemmus@users.noreply.github.com>
2023-09-18 18:20:10 +0300

    Update main.c

--- ExerciseSet4/Week5/Exercise14/main.c
+++ ExerciseSet4/Week5/Exercise14/main.c
@@ -13,3 +13,93 @@ Then program converts all letters to upper case (see toupper() – function).
 
 Then program opens the file for writing in text mode and writes all read lines to file and closes the file.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_CHAR 80
+#define MAX_LINE 100
+
+int main(void) {
+
+    char filename[MAX_CHAR];
+    char character;
+    char file_array[MAX_LINE][MAX_CHAR];
+
+    for(int i = 0; i < MAX_LINE; i++) {
+        for(int j = 0; j < MAX_CHAR; j++) {
+            file_array[i][j] = ' ';
+        }
+    }
+
+    // Asking for filename:
+    printf("Enter filename: ");
+    fgets(filename, MAX_CHAR, stdin);
+    filename[strcspn(filename, "\n")] = 0;
+
+    // Reading file:
+    FILE * fPtr;
+    fPtr = fopen(filename, "r");
+
+    if(fPtr == NULL)
+    {
+        printf("\nError. Unable to open file \"%s\" for reading. Bye!\n", filename);
+        exit(EXIT_FAILURE);
+    } else {
+        printf("\n\"%s\" opened successfully. Reading from file and putting input into file_array...\n", filename);
+
+        // Putting file into file_array:
+        do {
+            for(int i = 0; i < MAX_LINE; i++) {
+                for(int j = 0; j < MAX_CHAR; j++) {
+                    character = fgetc(fPtr);
+                    if ( character != '\n') {
+                        file_array[i][j] = character;
+                    } else {
+                        j = MAX_CHAR;
+                    }
+                }
+            }
+        } while (character != EOF);
+
+        fclose(fPtr);
+
+        printf("... done and file closed.\n\n");
+    }
+
+    // toupper():
+    for(int i = 0; i < MAX_LINE; i++) {
+        for(int j = 0; j < MAX_CHAR; j++) {
+            file_array[i][j] = toupper(file_array[i][j]);
+        }
+    }
+
+    // Writing to file:
+    fPtr = fopen(filename, "w");
+
+    if(fPtr == NULL)
+    {
+        printf("Error. Unable to open file \"%s\" for writing. Bye!\n", filename);
+        exit(EXIT_FAILURE);
+    } else {
+        printf("\"%s\" opened successfully. Writing to file from file_array...\n", filename);
+
+
+        for(int i = 0; i < MAX_LINE; i++) {
+            for(int j = 0; j < MAX_CHAR; j++) {
+                if(file_array[i][j] >= 32) {
+                    fprintf(fPtr, "%c",file_array[i][j]);
+                }
+            }
+            fprintf(fPtr, "\n");
+        }
+
+        fclose(fPtr);
+
+        printf("... done and file closed.\n");
+    }
+
+    return 0;
+}
